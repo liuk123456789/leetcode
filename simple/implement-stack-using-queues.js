eb7e5a8980eecfea5cc371ba481fd3125d822a5f
kona <liuer5194@gmail.com>
2024-09-27 14:36:16 +0800

    feat:update

--- simple/implement-stack-using-queues.js
+++ simple/implement-stack-using-queues.js
@@ -13,36 +13,33 @@
     你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
 */
 var MyStack = function() {
-    
+    this.queue1 = [];
+    this.queue2 = [];//备份的队列
 };
 
-/** 
- * @param {number} x
- * @return {void}
- */
 MyStack.prototype.push = function(x) {
-
+    this.queue1.push(x);
 };
 
-/**
- * @return {number}
- */
 MyStack.prototype.pop = function() {
-
+  	// 减少两个队列交换的次数， 只有当queue1为空时，交换两个队列
+    if(!this.queue1.length) {
+        [this.queue1, this.queue2] = [this.queue2, this.queue1];
+    }
+    while(this.queue1.length > 1) {//当队列1的元素数量大于1的时候不断将元素push进备份队列
+        this.queue2.push(this.queue1.shift());
+    }
+    return this.queue1.shift();//最后将队列1最后一个元素出队
 };
 
-/**
- * @return {number}
- */
 MyStack.prototype.top = function() {
-
+    const x = this.pop();//查看栈顶，队列出队，然后在push进队列1
+    this.queue1.push(x);
+    return x;
 };
 
-/**
- * @return {boolean}
- */
 MyStack.prototype.empty = function() {
-
+    return !this.queue1.length && !this.queue2.length;
 };
 
 /**
