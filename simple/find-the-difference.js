6f066755134bbaeed7e8269381f66ad8b44d236d
kona <liuer5194@gmail.com>
2024-10-23 18:22:13 +0800

    feat:update

--- simple/find-the-difference.js
+++ simple/find-the-difference.js
@@ -24,5 +24,39 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
+    const map = new Map()
+    for(let target of s) {
+        if(map.has(target)) {
+            const val = map.get(target)
+            map.set(target, val + 1)
+        } else {
+            map.set(target, 1)
+        }
+    }
 
+    for(let cur of t) {
+        if(map.has(cur)) {
+            const val = map.get(cur)
+            if(val - 1 < 0) {
+                return cur
+            } else {
+                map.set(cur, val - 1)
+            }
+        } else {
+            return cur
+        }
+    }
+};
+
+// 官方解法
+// 将两个字符串拼接成一个字符串，其中出现奇数次的必定就是混入字符
+var findTheDifference = function(s, t) {
+    let ret = 0;
+    for (const ch of s) {
+        ret ^= ch.charCodeAt();
+    }
+    for (const ch of t) {
+        ret ^= ch.charCodeAt();
+    }
+    return String.fromCharCode(ret);
 };
