1c59ffd762459b85370884fffc191f3ad9d40630
kona <liuer5194@gmail.com>
2024-11-26 18:49:30 +0800

    fix:等等等等

---
+++ simple/alternating-groups-i.js
@@ -0,0 +1,40 @@
+/**
+ * 给你一个整数数组 colors ，它表示一个由红色和蓝色瓷砖组成的环，第 i 块瓷砖的颜色为 colors[i] ：
+
+colors[i] == 0 表示第 i 块瓷砖的颜色是 红色 。
+colors[i] == 1 表示第 i 块瓷砖的颜色是 蓝色 。
+环中连续 3 块瓷砖的颜色如果是 交替 颜色（也就是说中间瓷砖的颜色与它 左边 和 右边 的颜色都不同），那么它被称为一个 交替 组。
+
+请你返回 交替 组的数目。
+
+注意 ，由于 colors 表示一个 环 ，第一块 瓷砖和 最后一块 瓷砖是相邻的。
+
+
+示例 1：
+
+输入：colors = [1,1,1]
+
+输出：0
+
+
+示例 2：
+
+输入：colors = [0,1,0,0,1]
+
+输出：3
+
+*/
+/**
+ * @param {number[]} colors
+ * @return {number}
+ */
+var numberOfAlternatingGroups = function (colors) {
+    let res = 0;
+    const len = colors.length
+    for (let i = 0; i < len; i++) {
+        if (colors[i] !== colors[(i - 1 + len) % len] && colors[i] !== colors[(i + 1 + len) % len]) {
+            res++
+        }
+    }
+    return res
+};
